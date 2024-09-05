a1c205ac9d0880775d3726eb21c9d63f6f5bad15
TehCK <70246626+TehCK@users.noreply.github.com>
2024-09-05 14:22:07 +0800

    Add files via upload
    video manager script being update.
    add in loading screen

--- unity_source_code/Assets/Scripts/IntroVideoManager.cs
+++ unity_source_code/Assets/Scripts/IntroVideoManager.cs
@@ -1,21 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.UI;
 
 public class IntroVideoManager : MonoBehaviour
 {
     public  VideoPlayer videoPlayer;
-    private string videoFileName = "intro_animation.mov";
+    // private string videoFileName = "/Intro_Animation.mp4";
+    // public VideoClip videoClip;
+    public GameObject loadingScreen; 
     private int targetSceneIndex;
 
     public void StartVideo(int sceneIndex)
     {
-        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
-        videoPlayer.url = videoPath;
-        videoPlayer.errorReceived += OnVideoError;
+        StartCoroutine(PlayVideo());
+        // string videoPath = Application.streamingAssetsPath + videoFileName;
+        // videoPlayer.url = videoPath;
+        // // videoPlayer.clip = videoClip;
+        // videoPlayer.errorReceived += OnVideoError;
         targetSceneIndex = sceneIndex; 
-        videoPlayer.Prepare();
-        videoPlayer.prepareCompleted += OnPrepareCompleted;
+        // videoPlayer.Prepare();
+        // videoPlayer.prepareCompleted += OnPrepareCompleted;
+    }
+
+    IEnumerator PlayVideo()
+    {
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, "Intro_Animation.mp4");
+
+        #if UNITY_WEBGL && !UNITY_EDITOR
+        videoPath = System.Uri.EscapeUriString(videoPath);
+        #endif
+
+        using (UnityWebRequest uwr = UnityWebRequest.Get(videoPath))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.LogError(uwr.error);
+            }
+            else
+            {
+                videoPlayer.url = videoPath;
+                videoPlayer.Prepare();
+                loadingScreen.SetActive(false);
+                OnPrepareCompleted(videoPlayer);
+            }
+        }
     }
 
     // void Start()
@@ -30,7 +63,26 @@ public class IntroVideoManager : MonoBehaviour
     void OnPrepareCompleted(VideoPlayer vp)
     {
         vp.Play();
+        StartCoroutine(CheckBuffering());
         vp.loopPointReached += OnVideoEnd;
+    }
+
+    IEnumerator CheckBuffering()
+    {
+        while (videoPlayer.isPrepared && (ulong)videoPlayer.frame < videoPlayer.frameCount - 10)
+        {
+            if (videoPlayer.isPlaying && videoPlayer.frame > 0)
+            {
+                yield return null;
+            }
+            else
+            {
+                Debug.Log("Buffering...");
+                videoPlayer.Pause();
+                yield return new WaitForSeconds(0.5f);
+                videoPlayer.Play();
+            }
+        }
     }
 
     void OnVideoError(VideoPlayer vp, string message)
